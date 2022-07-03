1f77235d3d30a92c91a790566d557102b653c445
jgabriel <jgabriel.souza11@gmail.com>
2022-07-03 14:46:37 -0300

    finished training

---
+++ 49 Program - Event object/myScript.js
@@ -0,0 +1,29 @@
+// Javascript - event object
+
+// get button by id
+var btn2Element = document.getElementById("btn2");
+
+function changeBtnFontSize(event) {
+
+    //this.style.fontSize = "40px";
+    event.target.style.fontSize = "40px";
+
+    alert(event.type);
+
+    alert(event.target);
+}
+
+btn2Element.addEventListener("click", changeBtnFontSize);
+
+//get element by id
+var aElement = document.getElementById("click-me");
+
+function clickMe(event) {
+
+    alert("You just clicked me!");
+
+    event.preventDefault();
+
+}
+
+aElement.addEventListener("click", clickMe);
---
+++ 7 Assignment/myScript.js
@@ -0,0 +1,8 @@
+var pElement = document.getElementsByClassName("my-p")[0];
+
+pElement.style.color = "blue";
+pElement.style.fontSize = "20px";
+pElement.style.border = "3px solid yellow";
+pElement.style.padding = "10px";
+
+pElement.className = "my-p";
